8765d947563d44e36c5426c4e90b4736ca057175
Helamã <cristian.pinheiro>
2023-12-13 23:42:07 -0300

    Added new booking tab

--- src/components/main-tabs/app-card-navigation.tsx
+++ src/components/main-tabs/app-card-navigation.tsx
@@ -1,52 +1,74 @@
 'use client';
+import CreateBookNavigation from '../create-book/create-book-navigation';
+import { AddIcon } from '../icons/generic-icons';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
 import CanceledBookings from './canceled-bookings';
 import CurrentBookings from './current-bookings';
 import HistoryBookings from './history-bookings';
 
-export type AppTabs = 'current' | 'history' | 'canceled';
-export interface AppTabListProps {
+export type AppTabs = 'current' | 'history' | 'canceled' | 'new';
+
+type IconTab = {
+    type: 'icon';
+    icon: JSX.Element;
     tab: AppTabs;
+};
+
+type TextTab = {
+    type: 'text';
     label: string;
-}
+    tab: AppTabs;
+};
+
+export type AppTabListProps = IconTab | TextTab;
+
+const a: AppTabListProps = {
+    tab: 'current',
+    type: 'text',
+    label: 'Current bookings',
+};
+
 const tabs: AppTabListProps[] = [
-    { tab: 'current', label: 'Current bookings' },
-    { tab: 'history', label: 'History' },
-    { tab: 'canceled', label: 'Canceled' },
+    { tab: 'current', label: 'Current bookings', type: 'text' },
+    { tab: 'history', label: 'History', type: 'text' },
+    { tab: 'canceled', label: 'Canceled', type: 'text' },
+    { tab: 'new', icon: <AddIcon />, type: 'icon' },
 ];
 
 const AppCardNavigation = ({ tab }: { tab: AppTabs }) => {
-    // const redirectUrl = (tab: AppTabs) => {
-    //     return `/app?tab=${tab}`;
-    // };
-
-    // const cleanGlobalStates = useSetAtom(cleanStatesAtom);
-
-    // useEffect(() => {
-    //     return () => {
-    //         cleanGlobalStates();
-    //     };
-    // }, [cleanGlobalStates]);
-
     return (
-        <section className="w-full bg-gray min-h-[40vh] rounded-md max-w-[800px]">
-            <Tabs defaultValue={tab} className="w-full px-2">
+        <section className="w-full bg-gray   rounded-md max-w-[800px]">
+            <Tabs defaultValue={tab} className="w-full px-2 py-3">
                 <TabsList className="w-full flex justify-between">
                     {tabs.map((t) => (
-                        <TabsTrigger className="w-[150px]" key={t.tab} value={t.tab}>
-                            {t.label}
+                        <TabsTrigger
+                            className={
+                                t.type === 'text' ? 'w-[150px] shadow-md' : 'p-1 rounded-full'
+                            }
+                            key={t.tab}
+                            value={t.tab}>
+                            {t.type === 'icon' ? (
+                                t.icon
+                            ) : (
+                                <span className="text-sm">{t.label}</span>
+                            )}
                         </TabsTrigger>
                     ))}
                 </TabsList>
-                <TabsContent value="current">
-                    <CurrentBookings />
-                </TabsContent>
-                <TabsContent value="history">
-                    <HistoryBookings />
-                </TabsContent>
-                <TabsContent value="canceled">
-                    <CanceledBookings />
-                </TabsContent>
+                <div className="h-[60vh] overflow-auto">
+                    <TabsContent value="current">
+                        <CurrentBookings />
+                    </TabsContent>
+                    <TabsContent value="history">
+                        <HistoryBookings />
+                    </TabsContent>
+                    <TabsContent value="canceled">
+                        <CanceledBookings />
+                    </TabsContent>
+                    <TabsContent value="new">
+                        <CreateBookNavigation />
+                    </TabsContent>
+                </div>
             </Tabs>
         </section>
     );
